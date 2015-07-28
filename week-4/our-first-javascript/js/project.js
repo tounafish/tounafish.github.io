0cfe4169898b981a61b5b95336a40e5dc47a40dc
Touna <tunadesigner@yahoo.com>
2015-07-27 21:40:15 -0400

    ff

---
+++ week-4/our-first-javascript/js/project.js
@@ -0,0 +1,18 @@
+document.write(5*23, "<br>", 4-2, "<br>", 42/6, "<br>", 7+8, "<br>");
+document.write("Zaphod Beeblebrox");
+alert("Little lifeforms, oh little lifeforms, where are you?");
+2+3;
+
+var name = "Michael Touna";
+document.write("My Name is "+name);
+name = "Awesome";
+document.write(name);
+
+document.write("<ul>");
+
+function jack(smith) {
+	document.write("<li>"+smith+"</li>")
+}
+
+jack("List Item 1");
+jack(4+4);
