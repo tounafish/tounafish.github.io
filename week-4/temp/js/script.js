8ea6e46357186ed83355783ab11e19a7cad12708
Touna <tunadesigner@yahoo.com>
2015-07-28 12:59:40 -0400

    w

--- week-4/temp/js/script.js
+++ week-4/temp/js/script.js
@@ -1,20 +1,20 @@
-document.getElementById("hot").onclick = switchGray;
+document.getElementById("blue").onclick = coldBlue;
 
-function switchGray() {
-  document.getElementById("all").style.color = "white";
-  document.getElementById("all").style.backgroundColor = "gray";
-}
-
-document.getElementById("lower").onclick = switchWhite;
-
-function switchWhite() {
-  document.getElementById("all").style.color = "black";
-  document.getElementById("all").style.backgroundColor = "white";
-}
-
-document.getElementById("raise").onclick = switchBlue;
-
-function switchBlue() {
+function coldBlue() {
   document.getElementById("all").style.color = "white";
   document.getElementById("all").style.backgroundColor = "blue";
+}
+
+document.getElementById("red").onclick = hotRed;
+
+function hotRed() {
+  document.getElementById("all").style.color = "yellow";
+  document.getElementById("all").style.backgroundColor = "red";
+}
+
+document.getElementById("gray").onclick = netGray;
+
+function netGray() {
+  document.getElementById("all").style.color = "orange";
+  document.getElementById("all").style.backgroundColor = "gray";
 }
