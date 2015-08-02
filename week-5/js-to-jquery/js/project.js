29e4438dd818179f16f5118d4d84e766b7ec7b8a
Touna <tunadesigner@yahoo.com>
2015-08-01 20:22:01 -0400

    3

--- week-5/js-to-jquery/js/project.js
+++ week-5/js-to-jquery/js/project.js
@@ -1,36 +1,3 @@
-document.getElementsByTagName("a")[0].onclick = changeRed;
-document.getElementsByTagName("a")[1].onclick = changeBlue;
-document.getElementsByTagName("a")[2].onclick = changeGreen;
-document.getElementsByTagName("a")[3].onclick = changePink;
-
-function changeRed() {
-  var paragraphs = document.getElementsByTagName("p");
-
-  for(var i = 0; i < paragraphs.length; i++) {
-    paragraphs[i].style.color = "red";
-  }
-}
-
-function changeBlue() {
-  var paragraphs = document.getElementsByTagName("p");
-
-  for(var i = 0; i < paragraphs.length; i++) {
-    paragraphs[i].style.color = "blue";
-  }
-}
-
-function changeGreen() {
-  var paragraphs = document.getElementsByTagName("p");
-
-  for(var i = 0; i < paragraphs.length; i++) {
-    paragraphs[i].style.color = "green";
-  }
-}
-
-function changePink() {
-  var paragraphs = document.getElementsByTagName("p");
-
-  for(var i = 0; i < paragraphs.length; i++) {
-    paragraphs[i].style.color = "pink";
-  }
-}
+$("a").click(function(){
+  $("p").css("color", $(this).html());
+});
