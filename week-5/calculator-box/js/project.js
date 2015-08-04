3ce1aca84558b576d155c871b68fc50205e87388
Touna <tunadesigner@yahoo.com>
2015-08-04 00:34:37 -0400

    a

--- week-5/calculator-box/js/project.js
+++ week-5/calculator-box/js/project.js
@@ -21,30 +21,90 @@ Ideas on how to make it work:
 	9. When "out"  is clicked, it resets value to zero.
 */
 
-var a10 = 10;
-var a20 = 20;
-var a30 = 30;
-var n10 = -10;
-var n20 = -20;
-var n30 = -30;
+var a10;
+var a20;
+var a30;
+var n10;
+var n20;
+var n30;
 var out;
 var red;
 var blue;
 
-document.getElementById("out").onclick = result;
+document.getElementById("out").onclick = reset;
+document.getElementById("a10").onclick = a10r;
+document.getElementById("a20").onclick = a20r;
+document.getElementById("a30").onclick = a30r;
+document.getElementById("n10").onclick = n10r;
+document.getElementById("n20").onclick = n20r;
+document.getElementById("n30").onclick = n30r;
+document.getElementById("blue").onclick = bluer;
+document.getElementById("red").onclick = redr;
 
-function result() {
-	a10 = document.getElementById("a10").value;
-	a20 = document.getElementById("a20").value;
-	a30 = document.getElementById("a30").value;
-	n10 = document.getElementById("n10").value;
-	n20 = document.getElementById("n20").value;
-	n30 = document.getElementById("n30").value;
-
-
-
+function a10r() {
+	a10 = 10;
+	a20 = 20;
+	a30 = 30;
+	n10 = -10;
+	n20 = -20;
+	n30 = -30;
+	if (document.getElementById("a10").onclick){
+		total = out + a10;
+	}
+	else if {
+		document.getElementById("a20").onclick = out + a20;
+	}
+	else if {
+		document.getElementById("a30").onclick = out + a30;
+	}
+	else if {
+		document.getElementById("n10").onclick = out + n10;
+	}
+	else if {
+		document.getElementById("n20").onclick = out + n20;
+	}
+	else if {
+		document.getElementById("n30").onclick = out + n30;
+	}
+	else if {
+		document.getElementById("blue").onclick = document.style.background ("#444");
+	}
+	else if {
+		document.getElementById("red").onclick = document.style.background ("#aaa");
+	}
+	else {
+		document.getElementById("out").onclick = null;
+	}
+	document.getElementById("out").innerHTML = total;
 }
 
 
+/*
+document.getElementById("submit").onclick = addStudent;
+  
+function addStudent() {
+  var name = document.getElementById("name").value;
+  var grade = document.getElementById("grade").value;
+  var seat = document.getElementById("seat").value;
+  createStudent(name, grade, seat);
+}
 
+function createStudent(n, g, s) {
+  if(n == "Joe Bliss") {
+     document.body.innerHTML = document.body.innerHTML+"<h2>The teacher is:</h2>";
+  }
+  else {
+    document.body.innerHTML = document.body.innerHTML+"<h2>The current student is:</h2>";
+  }
+   document.body.innerHTML = document.body.innerHTML+"<p>Name: "+n+"</p>";
+  
+  if (g < 2) {
+    document.body.innerHTML = document.body.innerHTML+"<p class=\"failing\">Grade: "+g+"</p>";    
+  }
+  else {
+    document.body.innerHTML = document.body.innerHTML+"<p>Grade: "+g+"</p>";
+  }
+  document.body.innerHTML = document.body.innerHTML+"<p>Seat Assignment: "+s+"</p>";
+}
+*/
 
