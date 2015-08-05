0460ba5005bce691015d85b128b6659e903540b2
Touna <tunadesigner@yahoo.com>
2015-08-05 18:54:19 -0400

    fd

--- week-5/dating-site/js/connect.js
+++ week-5/dating-site/js/connect.js
@@ -28,5 +28,5 @@ function submit() {
 	var email = document.getElementById("email").value;
 	var reason = document.getElementById("reason").value;
 	var comment = document.getElementById("comment").value;
-	document.getElementById("emailsent").innerHTML = "Email Sent! <br>"+"<h5>"+name+"<br>"+email+"<br>"+reason+"<br>"+comment+"</h5>";
+	document.getElementById("righty").innerHTML = "<h1>Email Sent! </h1><br>"+"<h3>"+name+"<br>"+email+"<br>"+reason+"<br>"+comment+"</h3>";
 }
