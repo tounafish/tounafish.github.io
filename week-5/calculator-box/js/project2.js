70312090aad35fe706f33de0290f467a57e44842
Touna <tunadesigner@yahoo.com>
2015-08-07 00:41:14 -0400

    1

--- week-5/calculator-box/js/project2.js
+++ week-5/calculator-box/js/project2.js
@@ -21,64 +21,73 @@ Ideas on how to make it work:
 	9. When "out"  is clicked, it resets value to zero.
 */
 
-var num10=10;
-var num20=20;
-var num30=30;
+var num1=1;
+var num2=2;
+var num3=3;
+var num4=4;
+var num5=5;
+var num6=6;
+var num7=7;
+var num8=8;
+var num9=9;
+var num0=0;
 var out=0;
-var red;
-var blue;
 var total;
 
-document.getElementById("out").onclick = reset;
-document.getElementById("a10").onclick = a10r;
-document.getElementById("a20").onclick = a20r;
-document.getElementById("a30").onclick = a30r;
-document.getElementById("n10").onclick = n10r;
-document.getElementById("n20").onclick = n20r;
-document.getElementById("n30").onclick = n30r;
-document.getElementById("blue").onclick = bluer;
-document.getElementById("red").onclick = redr;
+document.getElementById("clear").onclick = reset;
+document.getElementById("a1").onclick = a1r;
+document.getElementById("a2").onclick = a2r;
+document.getElementById("a3").onclick = a3r;
+document.getElementById("a4").onclick = a4r;
+document.getElementById("a5").onclick = a5r;
+document.getElementById("a6").onclick = a6r;
+document.getElementById("a7").onclick = a7r;
+document.getElementById("a8").onclick = a8r;
+document.getElementById("a9").onclick = a9r;
+document.getElementById("a0").onclick = a0r;
 
-function a10r() {
-	document.getElementById("out").innerHTML = out += num10;
+function a1r() {
+	document.getElementById("out").innerHTML = out += num1;
 }
 
-function a20r() {
-	document.getElementById("out").innerHTML = out += num20;
+function a2r() {
+	document.getElementById("out").innerHTML = out += num2;
 }
 
-function a30r() {
-	document.getElementById("out").innerHTML = out += num30;
+function a3r() {
+	document.getElementById("out").innerHTML = out += num3;
 }
 
-function n10r() {
-	document.getElementById("out").innerHTML = out -= num10;
+function a4r() {
+	document.getElementById("out").innerHTML = out += num4;
 }
 
-function n20r() {
-	document.getElementById("out").innerHTML = out -= num20;
+function a5r() {
+	document.getElementById("out").innerHTML = out += num5;
 }
 
-function n30r() {
-	document.getElementById("out").innerHTML = out -= num30;
+function a6r() {
+	document.getElementById("out").innerHTML = out += num6;
 }
 
-function redr() {
-	document.getElementById("out").style.background = "red";
-	document.getElementById("out").style.color = "white";
-
+function a7r() {
+	document.getElementById("out").innerHTML = out += num7;
 }
 
-function bluer() {
-	document.getElementById("out").style.background = "blue";
-	document.getElementById("out").style.color = "white";
+function a8r() {
+		document.getElementById("out").innerHTML = out += num8;
+}
 
+function a9r() {
+		document.getElementById("out").innerHTML = out += num9;
+}
+
+function a0r() {
+		document.getElementById("out").innerHTML = out += num0;
 }
 
 function reset() {
 	document.getElementById("out").innerHTML = "0";
-	document.getElementById("out").style.background = "white";
-	document.getElementById("out").style.color = "black";
-
+	total == 0;
 }
 
