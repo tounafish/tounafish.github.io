f94654091a86282a34e7ac2c0bca7bd80eb52779
Touna <tunadesigner@yahoo.com>
2015-08-11 21:56:48 -0400

    l

--- week-6/farm-party/js/projects2.js
+++ week-6/farm-party/js/projects2.js
@@ -1,11 +1,16 @@
 $( document ).ready(function() {
 	$(".moveit").hover(move);
+	$("div[id]").click(backgrounde);
 	});
 
+	function backgrounde(){
+		$(this).removeClass("display");
+	};
 	function move() {
-		$(this).animate({ "top": "90%", "left": "90%" }, 2000, complete);
+		$(this).animate({ "top": "0", "left": "1600px" }, 2000, complete);
 	};
 
 	function complete(){
-		$(this).animate({ "top": "0%", "left": "0%" }, 100);
-};
+		$(this).animate({ "top": "0", "left": "0" }, 0);
+	};
+
