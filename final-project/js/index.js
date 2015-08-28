ed1128e3e99183c635c057794d943342b4c0a049
Touna <tunadesigner@yahoo.com>
2015-08-28 15:28:22 -0400

    New Jquery for motion

--- final-project/js/index.js
+++ final-project/js/index.js
@@ -1,13 +1,28 @@
 $(document).ready(function () {
-    $("div.hidden").fadeIn(3000, function(){
+/*	   $("div.hidden").fadeIn(3000, function(){
 	    $("div.letter").animate({
 		    "top": "100%", 
 		    "left": "100%"
 		  }, 200);
     }).removeClass("hidden");
 
-    $("div h1").fadeOut(6000);
-   	/*$(".crazyidea").removeClass();*/
+    $("div h1").fadeOut(6000);*/
+
+
+	var qwer={
+		"a0":"a",
+		"a1":"b",
+		"a2":"c",
+		"a3":"d"
+	}
+setTimeout(function(){
+	var img = document.getElementsByTagName('img');
+	for(i=0;i<img.length;i++){
+		var index = "a"+Math.floor(Math.random()*4);
+		img[i].className=qwer[index];
+	}
+},1000)
+   	
 
    
 });
