1e6c2c7b20401d356033ed427187b21e11efd195
Touna <tunadesigner@yahoo.com>
2015-08-28 20:25:18 -0400

    ffs

--- final-project/js/boxes.js
+++ final-project/js/boxes.js
@@ -1,5 +1,21 @@
 $(document).ready(function() {
 
-
+	var qwer={
+		"a0":"a",
+		"a1":"b",
+		"a2":"c",
+		"a3":"d"
+	}
 	
-};
+setTimeout(function(){
+	var div = document.getElementsByTagName('div');
+	for(i=0;i<div.length;i++){
+		var index = "a"+Math.floor(Math.random()*4);
+		div[i].className=qwer[index];
+	}
+},1000)
+   	
+
+   
+	
+});
--- final-project/js/index.js
+++ final-project/js/index.js
@@ -8,19 +8,19 @@ $(document).ready(function () {
 
     $("div h1").fadeOut(6000);*/
 
-
 	var qwer={
 		"a0":"a",
 		"a1":"b",
 		"a2":"c",
 		"a3":"d"
 	}
-setTimeout(function(){
-	var img = document.getElementsByTagName('img');
-	for(i=0;i<img.length;i++){
-		var index = "a"+Math.floor(Math.random()*4);
-		img[i].className=qwer[index];
-	}
+
+	setTimeout(function(){
+		var img = document.getElementsByTagName('img');
+		for(i=0;i<img.length;i++){
+			var index = "a"+Math.floor(Math.random()*4);
+			img[i].className=qwer[index];
+		}
 },1000)
    	
 
