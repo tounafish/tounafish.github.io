54ddb9772989cf1ae6faaaba6ca51d50ac68688a
Touna <tunadesigner@yahoo.com>
2015-08-29 14:12:57 -0400

    adding meta

--- final-project/js/validate.js
+++ final-project/js/validate.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
 	$("formID").submit(function(evt){
 		evt.preventDefault(); //doesn't do the default action//
-		if($("input["))
+		if($("#username").val() != "jpg") {
+			alert("Not an image");
+			$("#username").css("background-color", "#fff");
+		}
+});
 
+	$("formID").focus (function(){
+		$(this).val("");
+		$(this).attr("style", "");
+	});
+
+	$("input").blur(function(){
+		if($(this).val() == "") {
+			alert("All fields are required");
+		}
+	});
 
 });
