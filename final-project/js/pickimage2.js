bd7b55ae6ae079298fd33e9d99a7910663304c44
Touna <tunadesigner@yahoo.com>
2015-09-01 13:04:29 -0400

    animation script

--- final-project/js/pickimage2.js
+++ final-project/js/pickimage2.js
@@ -11,52 +11,52 @@ $(document).ready(function() {
 
 	$(".imagex img").click(function(){	
 
-	    var ind = $('.boxsize');
+	   
+        var ind = $('.boxsize');
         var top = 0;
         var left = 0;
-
         for(i=0; i < ind.length; i++){
             ind[i].style.backgroundPosition = left+"px "+top+"px";
             if(i<7){
-                left = left+75;
+                left = left-75;
             }
             if(i==7){
-        		left = 0;
-       			top = 75;
+                left = 0;
+                top = -75;
             }
             if(i>7&&i<15){
-                left = left+75;
+                left = left-75;
             }
             if(i==15){
                 left = 0;
-                top = 150;
+                top = -150;
             }
             if(i>15&&i<23){
-                left = left+75;
+                left = left-75;
             }
             if(i==23){
                 left = 0;
-                top = 225;
+                top = -225;
             }
             if(i>23&&i<31){
-                left = left+75;
+                left = left-75;
             }
             if(i==31){
                 left = 0;
-                top = 300;
+                top = -300;
             }
             if(i>31&&i<39){
-                left = left+75;
+                left = left-75;
             }
             if(i==39){
                 left = 0;
-                top = 375;
+                top = -375;
             }
             if(i>39&&i<47){
-                left = left+75;
+                left = left-75;
             }
-
-        }
-    });
+        } 
+        
+    
 
 });
