3d08b95c86272a908283a76db5d55df4f3ee1bd3
Touna <tunadesigner@yahoo.com>
2015-09-08 13:16:33 -0400

    09

--- final-project/js/pickimage3.js
+++ final-project/js/pickimage3.js
@@ -3,51 +3,52 @@ $(document).ready(function() {
     // large images 1200px
 	$(".image70 img").on("click",function(){	
 		var src = $(this).attr("src");
-	   
         var ind = $(".boxsize");
         var top = 0;
         var left = 0;
         var percent = 0.5;
 
+        $(this).attr("width", "50%");
+
         for(i=0; i < ind.length; i++){
             ind[i].style.backgroundPosition = left+"px "+top+"px";
             if(i<7){
-                left = (left-75)*percent;
+                left = (left-75);
             }
             if(i==7){
                 left = 0;
                 top = -75;
             }
             if(i>7&&i<15){
-                left = (left-75)*percent;
+                left = (left-75);
             }
             if(i==15){
                 left = 0;
                 top = -150;
             }
             if(i>15&&i<23){
-                left = (left-75)*percent;
+                left = (left-75);
             }
             if(i==23){
                 left = 0;
                 top = -225;
             }
             if(i>23&&i<31){
-                left = (left-75)*percent;
+                left = (left-75);
             }
             if(i==31){
                 left = 0;
                 top = -300;
             }
             if(i>31&&i<39){
-                left = (left-75)*percent;
+                left = (left-75);
             }
             if(i==39){
                 left = 0;
                 top = -375;
             }
             if(i>39&&i<47){
-                left = (left-75)*percent;
+                left = (left-75);
             }
         } 
 
@@ -66,45 +67,47 @@ $(document).ready(function() {
         var left = 0;
         var percent = 0.66666667;
 
+        $(this).attr("width", "66%");
+
         for(i=0; i < ind.length; i++){
             ind[i].style.backgroundPosition = left+"px "+top+"px";
             if(i<7){
-                left = (left-75)*percent;
+                left = (left-75);
             }
             if(i==7){
                 left = 0;
                 top = -75;
             }
             if(i>7&&i<15){
-                left = (left-75)*percent;
+                left = (left-75);
             }
             if(i==15){
                 left = 0;
                 top = -150;
             }
             if(i>15&&i<23){
-                left = (left-75)*percent;
+                left = (left-75);
             }
             if(i==23){
                 left = 0;
                 top = -225;
             }
             if(i>23&&i<31){
-                left = (left-75)*percent;
+                left = (left-75);
             }
             if(i==31){
                 left = 0;
                 top = -300;
             }
             if(i>31&&i<39){
-                left = (left-75)*percent;
+                left = (left-75);
             }
             if(i==39){
                 left = 0;
                 top = -375;
             }
             if(i>39&&i<47){
-                left = (left-75)*percent;
+                left = (left-75);
             }
         } 
 
@@ -184,11 +187,14 @@ $(document).ready(function() {
                 ind[i].className = ind[i].className + " "+ x[Math.floor(Math.random()*6)]
             }
         },2000)
-            });
-
 
         //shows reset button after explosion
         $(".reset").removeClass();
+            });
+
+
+        
+        
 
     
 });
