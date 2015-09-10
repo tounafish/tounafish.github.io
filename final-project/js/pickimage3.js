8704266666c42603591dfadfb3b1773c561080ae
Touna <tunadesigner@yahoo.com>
2015-09-10 08:29:53 -0400

    awesomeness

--- final-project/js/pickimage3.js
+++ final-project/js/pickimage3.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+
+
     var speed = 8;
  
     /*speed = prompt("Enter speed");*/
@@ -191,6 +193,75 @@ $(document).ready(function() {
         $("#logo").css("display", "none");
         $(".box-footer").addClass("hidden");
         });
+
+
+
+
+
+    //User Image URL input
+    $("#clickme").on("click",function(){    
+        var src = $("#userurl").val();
+        var ind = $(".boxsize");
+        var top = 0;
+        var left = 0;
+
+        for(i=0; i < ind.length; i++){
+            ind[i].style.backgroundPosition = left+"px "+top+"px";
+            if(i<7){
+                left = (left-75);
+            }
+            if(i==7){
+                left = 0;
+                top = -75;
+            }
+            if(i>7&&i<15){
+                left = (left-75);
+            }
+            if(i==15){
+                left = 0;
+                top = -150;
+            }
+            if(i>15&&i<23){
+                left = (left-75);
+            }
+            if(i==23){
+                left = 0;
+                top = -225;
+            }
+            if(i>23&&i<31){
+                left = (left-75);
+            }
+            if(i==31){
+                left = 0;
+                top = -300;
+            }
+            if(i>31&&i<39){
+                left = (left-75);
+            }
+            if(i==39){
+                left = 0;
+                top = -375;
+            }
+            if(i>39&&i<47){
+                left = (left-75);
+            }
+        } 
+
+        //loads image, resizes and scrolls to top
+        $(".boxsize").css("background-image","url("+src+")");
+        $(".boxsize").css("background-size", "600px");
+        $("body").animate({scrollTop: 0}, "slow");
+
+        //show detonator and hides logo
+        $("#detonator").css("display", "block");
+        $("#logo").css("display", "none");
+        $(".box-footer").addClass("hidden");
+        });
+
+
+
+
+
 
 
 
