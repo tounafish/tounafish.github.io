9d6a0749a3948ff4df9fdcbd3ee1e9a89ceee54b
Touna <tunadesigner@yahoo.com>
2015-09-12 13:27:01 -0400

    week10

---
+++ final-project/js/loop.js
@@ -0,0 +1,42 @@
+$document.ready(function(){
+	for(i=1; i < 10; i++){
+        console.log(i);
+});
+
+
+
+WHILE LOOP
+function numbers (){
+	var i = 1;
+	while (i <= 10) {
+		console.log(i);
+		i += 1;
+	}
+}
+
+
+function numbers (){
+	var i = 2;
+	while (i <= 60) {
+		console.log(i);
+		i += 2 ;
+	}
+}
+
+
+var i = 1;
+while (i <= 60) {
+	if (i % 2 === 0) {
+		console.log(i);
+	}
+	i += 1;
+}
+
+
+
+
+
+For loop
+
+
+
