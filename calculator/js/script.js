e1968b660b05baa050256f89f8ef31435729f949
Touna <tunadesigner@yahoo.com>
2015-09-21 19:26:19 -0400

    e

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -1,15 +1,34 @@
 $(document).ready(function() {
 	var RunningTotal = 0;
 	var GrandTotal = 0;
+	var subtotal = false;
+	var x;
 
 	$(".white").click(function(){
 		/*var num = $(this).val();*/
-		var num = $(this).text(); 
-		if($(".answerbox").html()==0){
-			$(".answerbox").html(num);
-		}else{
-			$(".answerbox").html($(".answerbox").html()+num);
+		if(!subtotal){
+			var num = $(this).text(); 
+			if($(".answerbox").html()==0){
+				$(".answerbox").html(num);
+			}else{
+				$(".answerbox").html($(".answerbox").html()+num);
+			}
+		}else if(subtotal){
+			var num = $(this).text(); 
+			if($(".answerbox").html()==0){
+				$(".answerbox").html(num);
+			}else{
+				$(".answerbox").html($(".answerbox").html()+num);
+			}
 		}
+		
+	});
+
+	$(".orange").click(function(){
+		subtotal=true;
+		x=$('.answerbox').html();
+		$('.answerbox').html('');
+
 	});
 
 
